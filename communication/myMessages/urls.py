2f4caa7547b06ab5e0e632536e1e72d595ab50b1
Hubert <burzynskihubert@gmail.com>
2023-04-17 21:18:11 +0200

    init and create urls

---
+++ communication/myMessages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('messages/id/<conversation_id>', views.send_message, name='send_message'),
+    path('messages/my', views.my_conversations, name='my_messages'),
+]
---
+++ communication/myMessages/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+
+def send_message(request, conversation_id):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        # Do something with the message, such as sending it to a chatbot or saving it to a database.
+        response_data = {'status': 'success', 'conversation id': 'conversation_id'}
+        return JsonResponse(response_data)
+    else:
+        response_data = {'status': 'error', 'conversation id': conversation_id}
+        return JsonResponse(response_data, status=400)
+
+
+def my_conversations(request):
+    if request.method == 'GET':
+        response_data = {'status': 'success', 'message': 'My conversations'}
+        return JsonResponse(response_data, status=400)
